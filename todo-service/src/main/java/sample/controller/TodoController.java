56af37d529e3704442436b54cbf60107ff423636
nakamura <nakamura@genuine-pt.jp>
2017-04-07 17:41:55 +0900

    引数を受け取れるように@RequestBody追加

--- todo-service/src/main/java/sample/controller/TodoController.java
+++ todo-service/src/main/java/sample/controller/TodoController.java
@@ -2,6 +2,7 @@ package sample.controller;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +14,7 @@ public class TodoController {
 
 	@CrossOrigin(origins="http://localhost:4200")
 	@RequestMapping(value="/api/todo", method=RequestMethod.POST)
-	public TodoId createTodo(String text){
+	public TodoId createTodo(@RequestBody String text){
 		TodoId id = new TodoId();
 
 		//TODO: TODO作成コマンド発行
